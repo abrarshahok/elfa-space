e6a9856ef27532dc55ab337848e6ed3be94ae6f6
Abrar Ahmed Shahok <abrarahmedshahok12@gmail.com>
2023-11-09 22:24:03 +0500

    Some Fixes

--- lib/features/splash_screen/domain/utilities/utils.dart
+++ lib/features/splash_screen/domain/utilities/utils.dart
@@ -1,27 +1,30 @@
 import 'dart:async';
-
-import 'package:elfa_main_dashboard/features/enableLoction_screen/presentation/pages/enable_location_screen.dart';
-import 'package:elfa_main_dashboard/news_feed/app/app_drawer.dart';
+import '/features/carsouelSlide/presentation/pages/carsouel_slide.dart';
+import '/features/enableLoction_screen/presentation/pages/enable_location_screen.dart';
+import '/news_feed/app/app_drawer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import '../../presentation/provider/splash_screen_animator.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class Utils {
   void splashService(BuildContext context) {
+    final navigator = Navigator.of(context);
     Timer(const Duration(seconds: 3), () {
       Provider.of<SplashScreenAnimator>(context, listen: false)
           .triggerAnimation();
 
-      Timer(const Duration(milliseconds: 400), () {
+      Timer(const Duration(milliseconds: 400), () async {
         User? user = FirebaseAuth.instance.currentUser;
-
+        final permissionStatus = await Permission.location.status;
         if (user != null) {
-          Navigator.pushReplacementNamed(context, AppDrawer.routeName);
+          navigator.pushReplacementNamed(AppDrawer.routeName);
+        } else if (!permissionStatus.isGranted) {
+          navigator.pushReplacementNamed(EnableLocationScreen.routeName);
         } else {
-          Navigator.pushReplacementNamed(
-              context, EnableLocationScreen.routeName);
+          navigator.pushReplacementNamed(CarsouelSlide.routeName);
         }
       });
     });
